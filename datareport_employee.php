bc44908601e3cdfc6923af49f40a8004f84ddd35
Max O <134850941+MaxWOue@users.noreply.github.com>
2024-03-12 16:04:46 -0500

    Added button semi-functionality

--- datareport_employee.php
+++ datareport_employee.php
@@ -7,38 +7,40 @@
   </head>
   <body>
 	<h2>Employee Search</h2>
-	<label>First Name: </label><input id="DataReportFirstName" type="text">
-	<label>Last Name: </label><input id="DataReportLastName" type="text"><label><br></label>
-	<label>ID: </label><input id="DataReportEmployeeID" type="text">
-	<label>Position: </label><input id="DataReportPosition" type="text"><label><br></label>
-	<label>UserName: </label><input id="DataReportUserName" type="text">
-	<label>Supervisor: </label><input id="DataReportSupervisor" type="text"><label><br></label>
-	<label>Work Days:<br></label>
-	<input type="checkbox" id="WorksOnMonday"><label>Monday</label>
-	<input type="checkbox" id="WorksOnTuesday"><label>Tuesday</label>
-	<input type="checkbox" id="WorksOnWednesday"><label>Wednesday</label>
-	<input type="checkbox" id="WorksOnThursday"><label>Thursday</label>
-	<input type="checkbox" id="WorksOnFriday"><label>Friday</label>
-	<input type="checkbox" id="WorksOnSaturday"><label>Saturday</label>
-	<input type="checkbox" id="WorksOnSunday"><label>Sunday</label><br>
-	<button>Search</button><p></p>
+	<form id="form_datareportemployees" method="post">
+		<label>First Name: </label><input id="DataReportFirstName" type="text">
+		<label>Last Name: </label><input id="DataReportLastName" type="text"><label><br></label>
+		<label>ID: </label><input id="DataReportEmployeeID" type="text">
+		<label>Position: </label><input id="DataReportPosition" type="text"><label><br></label>
+		<label>UserName: </label><input id="DataReportUserName" type="text">
+		<label>Supervisor: </label><input id="DataReportSupervisor" type="text"><label><br></label>
+		<label>Work Days:<br></label>
+		<input type="checkbox" id="WorksOnMonday"><label>Monday</label>
+		<input type="checkbox" id="WorksOnTuesday"><label>Tuesday</label>
+		<input type="checkbox" id="WorksOnWednesday"><label>Wednesday</label>
+		<input type="checkbox" id="WorksOnThursday"><label>Thursday</label>
+		<input type="checkbox" id="WorksOnFriday"><label>Friday</label>
+		<input type="checkbox" id="WorksOnSaturday"><label>Saturday</label>
+		<input type="checkbox" id="WorksOnSunday"><label>Sunday</label><br>
+		<input type="submit" id="button" value="Search" name="datareport_employee_submitbutton"><br><br>
+	</form>
 	<?php
 		$servername = "database-1.cfkociaic7f3.us-east-2.rds.amazonaws.com";
 		$username = "admin";
 		$password = "josephjoestar";
 		$dbname = "MfahDB";
-		
-		$conn = mysqli_connect($servername, $username, $password, $dbname);
-		$sqlDataReport = 'SELECT FirstName, LastName FROM employee WHERE EmployeeID LIKE "%%" AND Postion LIKE "%%" AND FirstName LIKE "%%" AND LastName LIKE "%%" AND UserName LIKE "%%" AND Supervisor LIKE "%%" ORDER BY lastName, firstName';
-		$dataReportVtable = mysqli_query($conn, $sqlDataReport);
 
-		while ($row = mysqli_fetch_assoc($dataReportVtable)) {
-			echo $row['FirstName'] . " " . $row['LastName']
-			//echo "Name: " . $row['FirstName'] . " " . $row['LastName'] . "<br>";
+		if(isset($_POST['datareport_employee_submitbutton'])) {
+			$conn = mysqli_connect($servername, $username, $password, $dbname);
+			$sqlDataReport = 'SELECT FirstName, LastName FROM employee WHERE EmployeeID LIKE "%%" AND Postion LIKE "%%" AND FirstName LIKE "%%" AND LastName LIKE "%%" AND UserName LIKE "%%" AND Supervisor LIKE "%%" ORDER BY lastName, firstName';
+			$dataReportVtable = mysqli_query($conn, $sqlDataReport);
+
+			while ($row = mysqli_fetch_assoc($dataReportVtable)) {
+				//echo $row['FirstName'] . " " . $row['LastName'] . ;
+				echo "Name: " . $row['FirstName'] . " " . $row['LastName'] . "<br>";
+			}
 		}
-
 		echo "Hello Echo";
-
 	?>
   </body>
  </html>
