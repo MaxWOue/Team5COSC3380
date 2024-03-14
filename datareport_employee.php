9ce2ce8c3e1e382950af4e02282a19546a805ce6
Max O <134850941+MaxWOue@users.noreply.github.com>
2024-03-13 19:35:36 -0500

    Data Employee Currently Finished

--- datareport_employee.php
+++ datareport_employee.php
@@ -9,11 +9,11 @@
 	<h2>Employee Search</h2>
 	<form id="form_datareportemployees" method="post">
 		<label>First Name: </label><input name="DataReportFirstName" type="text">
-		<label>Last Name: </label><input id="DataReportLastName" type="text"><label><br></label>
-		<label>ID: </label><input id="DataReportEmployeeID" type="text">
-		<label>Position: </label><input id="DataReportPosition" type="text"><label><br></label>
-		<label>UserName: </label><input id="DataReportUserName" type="text">
-		<label>Supervisor: </label><input id="DataReportSupervisor" type="text"><label><br></label>
+		<label>Last Name: </label><input name="DataReportLastName" id="DataReportLastName" type="text"><label><br></label>
+		<label>ID: </label><input name="DataReportEmployeeID" id="DataReportEmployeeID" type="text">
+		<label>Position: </label><input name="DataReportPosition" id="DataReportPosition" type="text"><label><br></label>
+		<label>UserName: </label><input name="DataReportUserName" id="DataReportUserName" type="text">
+		<label>Supervisor: </label><input name="DataReportSupervisor" id="DataReportSupervisor" type="text"><label><br></label>
 		<label>Work Days:<br></label>
 		<input type="checkbox" id="WorksOnMonday"><label>Monday</label>
 		<input type="checkbox" id="WorksOnTuesday"><label>Tuesday</label>
@@ -31,18 +31,25 @@
 		$password = "josephjoestar";
 		$dbname = "MfahDB";
 		$searchedfirstname = $_POST['DataReportFirstName'];
+		$searchedlastname = $_POST['DataReportLastName'];
+		$searchedidasstr = $_POST['DataReportEmployeeID'];
+		$searchedidasint = intval($searchedidasstr);
+		$searchedposition = $_POST['DataReportPosition'];
+		$searchusername = $_POST['DataReportUserName'];
+		$searchsupervisor = $_POST['DataReportSupervisor'];
 		echo $searchedfirstname;
 
 		if(isset($_POST['datareport_employee_submitbutton'])) {
 			$conn = mysqli_connect($servername, $username, $password, $dbname);
-			$sqlDataReport = 'SELECT FirstName, LastName FROM employee WHERE EmployeeID LIKE "%%" AND Postion LIKE "%%" AND FirstName LIKE "%' . $searchedfirstname . '%" AND LastName LIKE "%%" AND UserName LIKE "%%" AND Supervisor LIKE "%%" ORDER BY lastName, firstName';
+			$sqlDataReport = 'SELECT FirstName, LastName, EmployeeID, Supervisor, Postion, UserName FROM employee WHERE EmployeeID=' . $searchedidasint . ' AND Postion LIKE "%' . $searchedposition . '%" AND FirstName LIKE "%' . $searchedfirstname . '%" AND LastName LIKE "%' . $searchedlastname . '%" AND UserName LIKE "%' . $searchusername . '%" AND Supervisor LIKE "%' . $searchsupervisor . '%" ORDER BY lastName, firstName';
+
 			$dataReportVtable = mysqli_query($conn, $sqlDataReport);
+			//EmployeeID=' . intval($DataReportEmployeeID)
 
 			while ($row = mysqli_fetch_assoc($dataReportVtable)) {
 				//echo $row['FirstName'] . " " . $row['LastName'] . ;
-				echo "Name: " . $row['FirstName'] . " " . $row['LastName'] . "<br>";
+				echo "=" . $row['FirstName'] . " " . $row['LastName'] . "=<br>Employee ID: " . $row['EmployeeID'] . "<br>Supervisor ID: " . $row['Supervisor'] . "<br>Position: " . $row['Postion'] . "<br>Employee User Name: " . $row['UserName'] . "<br><br>";
 			}
 		}
-		echo "Test";
 	?>
  </html>
